0202de98709e7b60ecff42291b2aa3760fb15d06
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-06-04 14:31:25 -0300

    mongodb no docker

--- src/config/database.js
+++ src/config/database.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect('mongodb://admin:ogfy1ixfqxz@10.1.1.10/todo?authSource=admin')
